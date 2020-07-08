a1be28173df4a0920a872ba2c92c66c3caee83e3
richknicks <rlynch@gmail.com>
2020-07-08 17:54:41 -0400

    starting Day 3

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,13 +1,14 @@
-# Your code here
+# Your code here.
 
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
-    v //= (x + y)
+    v //= x + y
     v %= 982451653
 
     return v
+
 
 def slowfun(x, y):
     """
@@ -17,10 +18,9 @@ def slowfun(x, y):
     # Your code here
 
 
-
 # Do not modify below this line!
 
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f"{i}: {x},{y}: {slowfun(x, y)}")
